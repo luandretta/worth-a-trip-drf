f09e318622789a8d3b274d7109fab08bb48ba798
Lucimeri Andretta <luandretta@hotmail.com>
2023-07-18 09:39:04 +0000

    Feat: add signal to profile model
    Feat(admin): Register profile model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+# Register the models
+admin.site.register(Profile)
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -44,3 +45,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
