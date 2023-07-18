daed7e20110f7aca929506463d6ca54f74d8c9e4
Lucimeri Andretta <luandretta@hotmail.com>
2023-07-18 12:30:47 +0000

    Feat:create custom permissions
    Connect the custom permissions to the profile detail view
    
    Pass the request data from views.py as a context value for each method

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializer.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'name', 'bio', 'created_at', 'updated_at',
-            'profile_pic', 'bg_pic', 'birth_date', 'location'
+            'profile_pic', 'bg_pic', 'birth_date', 'location', 'is_owner'
         ]
