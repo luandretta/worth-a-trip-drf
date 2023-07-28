6a1000561a7e41dba1f7fbeffcab8eabf1269b48
Lucimeri Andretta <luandretta@hotmail.com>
2023-07-28 08:23:01 +0000

    Fix: on_delete in Wish model
    Reset db.sqlite database again due to slug field in post model and id in profile model, then migrate

--- wishes/migrations/0001_initial.py
+++ wishes/migrations/0001_initial.py
@@ -16,17 +16,16 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Comment',
+            name='Wish',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('content', models.TextField()),
                 ('owner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='posts.post')),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='wishes', to='posts.post')),
             ],
             options={
                 'ordering': ['-created_at'],
+                'unique_together': {('owner', 'post')},
             },
         ),
     ]
--- wishes/models.py
+++ wishes/models.py
@@ -4,8 +4,14 @@ from posts.models import Post
 
 
 class Wish(models.Model):
-    owner = models.ForeignKey(User, on_delete=CASCADE)
-    post = models.ForeignKey(Post, related_name='wishes', on_delete=CASCADE)
+    """
+    Wish model related to 'owner' and 'post'
+    'owner' is a User isntance and 'post' is a Post instance
+    'unique_together' makes sure a user can't wish the same post twice
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='wishes', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
