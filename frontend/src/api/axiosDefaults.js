2851153899f7eab4f81b6e5d10fb65c1a297368f
Lucimeri Andretta <luandretta@hotmail.com>
2023-08-11 08:48:26 +0000

    Add route to serve the index template

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://worth-a-trip-drf-40e2fa952827.herokuapp.com/';
+axios.defaults.baseURL = '/api';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
