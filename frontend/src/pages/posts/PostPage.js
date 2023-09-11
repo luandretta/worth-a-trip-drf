971c4abdc31ba2dd1e6e4b022a0336a230e9e8df
Lucimeri Andretta <luandretta@hotmail.com>
2023-09-11 07:13:32 +0000

    Feat: add structute to display comments under a post

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -21,11 +21,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data:comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -51,6 +52,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ?  (
+            comments.results.map(comment => (
+              <p key={comment.id}>
+                {comment.owner}:{comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
         </Container>
       </Col>
     </Row>
