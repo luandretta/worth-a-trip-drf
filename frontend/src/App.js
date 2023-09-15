fe2052f901d7b3f6cb6f6e289edc4e1fd6f0c5be
Lucimeri Andretta <luandretta@hotmail.com>
2023-09-15 07:06:17 +0000

    Feat: create NotFound component
    Add route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -96,7 +97,7 @@ function App() {
               path="/profiles/:id/edit"
               render={() => <ProfileEditForm />}
             />
-            <Route render={() => <p>Page not found :/</p>} />
+            <Route render={() => <NotFound />} />
           </Switch>
         </Container>
     </div>
