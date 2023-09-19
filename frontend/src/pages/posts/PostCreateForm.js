11a92d586115099a96a90100031994e877b93775
Lucimeri Andretta <luandretta@hotmail.com>
2023-09-19 16:35:39 +0000

    Feat(PostCreateForm): add the local_security, infrastructure, local_population an local_acces
    Create the handleRating in PostCreateForm

--- frontend/src/pages/posts/PostCreateForm.js
+++ frontend/src/pages/posts/PostCreateForm.js
@@ -13,6 +13,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
 // Image
 import Upload from "../../assets/upload.png";
 // Styles
@@ -23,6 +24,8 @@ import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 // Notifications
 import { NotificationManager } from "react-notifications";
+// React components
+import { Rating } from "react-simple-star-rating";
 
 // Component used for creating a post
 // Takes input from the user in the forms and post it to the API
@@ -42,8 +45,21 @@ function PostCreateForm() {
     image: "",
     trip_type: "unknown",
   });
-  // Destructuring the values of title, about and image from the postData object
+  // Destructuring the values  from the postData object
   const { title, country, location, content, image, trip_type } = postData;
+  // Setting the initial rating value
+  const [local_security, setLocal_security] = useState(0);
+  const [infrastructure, setInfrastructure] = useState(0);
+  const [local_population, setLocal_population] = useState(0);
+  const [local_access, setLocal_access] = useState(0);
+
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setLocal_security(rate / 20);
+    setInfrastructure(rate / 20);
+    setLocal_population(rate / 20);
+    setLocal_access(rate / 20);
+  };
 
   const imageInput = useRef(null);
   // Using the useHistory hook to handle navigation history
@@ -79,6 +95,10 @@ function PostCreateForm() {
     formData.append("content", content);
     formData.append("image", imageInput.current.files[0]);
     formData.append("trip_type", trip_type);
+    formData.append("local_access", local_access);
+    formData.append("infrastructure", infrastructure);
+    formData.append("local_security", local_security);
+    formData.append("local_population", local_population);
     // Append the data and request the post request from the API
     try {
       const { data } = await axiosReq.post("/posts/", formData);
@@ -134,7 +154,7 @@ function PostCreateForm() {
           </Form.Control>
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Label htmlFor="location">Location</Form.Label>
+          <Form.Label htmlFor="location">City</Form.Label>
           <Form.Control
             type="text"
             id="location"
@@ -172,9 +192,38 @@ function PostCreateForm() {
           </Form.Control>
         </Form.Group>
       </Form.Row>
+      <div className="text-center" md={5}>
+      <h5>Rate:</h5>
+        <Row className="justify-content-md-center m-1 mt-1" as={Col}>
+          <Card.Text>
+            Local access:
+            <Rating onClick={handleRating} />
+          </Card.Text>
+        </Row>
+        <Row className="justify-content-md-center m-1" as={Col}>
+          <Card.Text>
+            Infrastructure:
+            <Rating onClick={handleRating} />
+          </Card.Text>
+        </Row>
+        <Row className="justify-content-md-center m-1" as={Col}>
+          <Card.Text>
+            Local security:
+            <Rating onClick={handleRating} />
+          </Card.Text>
+        </Row>
+        <Row className="justify-content-md-center m-1" as={Col}>
+          <Card.Text>
+            Local population:
+            <Rating onClick={handleRating} />
+          </Card.Text>
+        </Row>
+      </div>
 
       <Form.Group>
-        <Form.Label htmlFor="content">Share more important information</Form.Label>
+        <Form.Label className="mt-2" htmlFor="content">
+          <h5>Share more important information</h5>
+        </Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
@@ -208,8 +257,9 @@ function PostCreateForm() {
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={12}>
           <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center text-center mt-3`}
           >
+            <h5>Create your own post!</h5>
             <Form.Group className="text-center" lg={10}>
               {image ? (
                 <>
@@ -251,9 +301,8 @@ function PostCreateForm() {
               </Alert>
             ))}
 
-            <div className="d-md-none">{textFields}</div>
+            <div>{textFields}</div>
           </Container>
-          <Container className={appStyles.Content}>{textFields}</Container>
         </Col>
       </Row>
     </Form>
