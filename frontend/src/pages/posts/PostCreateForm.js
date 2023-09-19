8e16456b8568f9c1a62691e279d62583036a057d
Lucimeri Andretta <luandretta@hotmail.com>
2023-09-19 07:33:06 +0000

    Add NotificationManager and comments into PostCreateForm
    Test the notifications creating new post and works like a charm

--- frontend/src/pages/posts/PostCreateForm.js
+++ frontend/src/pages/posts/PostCreateForm.js
@@ -1,5 +1,11 @@
+// React / Router
 import React, { useRef, useState } from "react";
-
+import { useHistory } from "react-router";
+// API
+import { axiosReq } from "../../api/axiosDefaults";
+// Hooks
+import { useRedirect } from "../../hooks/useRedirect";
+// React Bootstrap components
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
@@ -7,39 +13,43 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
-
+// Image
 import Upload from "../../assets/upload.png";
-
+// Styles
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+// Component
 import Asset from "../../components/Asset";
-
-import { useHistory } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useRedirect } from "../../hooks/useRedirect";
+// Notifications
+import { NotificationManager } from "react-notifications";
 
 // Component used for creating a post
 // Takes input from the user in the forms and post it to the API
 // Includes error handling that shows an alert to the user
 
 function PostCreateForm() {
+  // Using the useRedirect hook to redirect if the user is logged out
   useRedirect("loggedOut");
+  // Setting the initial state of the errors object to an empty object
   const [errors, setErrors] = useState({});
-
+  // Setting the initial state of the postData object with empty strings
   const [postData, setPostData] = useState({
     title: "",
-    country: [],
+    country: "",
     location: "",
     content: "",
     image: "",
     trip_type: "unknown",
   });
+  // Destructuring the values of title, about and image from the postData object
   const { title, country, location, content, image, trip_type } = postData;
 
   const imageInput = useRef(null);
+  // Using the useHistory hook to handle navigation history
   const history = useHistory();
 
+  // Handling input changes and updating the postData object
   const handleChange = (event) => {
     setPostData({
       ...postData,
@@ -47,6 +57,7 @@ function PostCreateForm() {
     });
   };
 
+  // Functionality to change the image
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
@@ -57,6 +68,7 @@ function PostCreateForm() {
     }
   };
 
+  // Handling the form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
@@ -67,58 +79,70 @@ function PostCreateForm() {
     formData.append("content", content);
     formData.append("image", imageInput.current.files[0]);
     formData.append("trip_type", trip_type);
-
+    // Append the data and request the post request from the API
     try {
       const { data } = await axiosReq.post("/posts/", formData);
       history.push(`/posts/${data.id}`);
+      // Display success notification
+      NotificationManager.success("Post created successfully", "Success!");
     } catch (err) {
       // console.log(err);
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
+        // Display error notification
+        NotificationManager.error(
+          "There was an issue adding your post",
+          "Error"
+        );
       }
     }
   };
 
+  // Text Fields
   const textFields = (
     <div className="text-center" md={5}>
       <Form.Group>
-        <Form.Label>Title</Form.Label>
+        <Form.Label htmlFor="title">Title</Form.Label>
         <Col>
           <Form.Control
             type="text"
+            id="title"
             name="title"
             value={title}
             onChange={handleChange}
           />
         </Col>
       </Form.Group>
+      {/* Displaying any title errors */}
       {errors?.title?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
       ))}
       <Form.Row>
-      <Form.Group as={Col}>
-          <Form.Label>Country</Form.Label>
+        <Form.Group as={Col}>
+          <Form.Label htmlFor="country">Country</Form.Label>
           <Form.Control
             as="select"
             type="text"
+            id="country"
             name="country"
             value={country.name}
             onChange={handleChange}
           >
-            <option value={country.name}>Country</option>
-            
+            <option value={[country.name]}>Choose one</option>
           </Form.Control>
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Label>Location</Form.Label>
+          <Form.Label htmlFor="location">Location</Form.Label>
           <Form.Control
             type="text"
+            id="location"
             name="location"
             value={location}
             onChange={handleChange}
           />
+          {/* Displaying any about errors */}
           {errors?.location?.map((message, idx) => (
             <Alert variant="warning" key={idx}>
               {message}
@@ -126,10 +150,11 @@ function PostCreateForm() {
           ))}
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Label>Trip type</Form.Label>
+          <Form.Label htmlFor="trip_type">Trip type</Form.Label>
           <Form.Control
             as="select"
             type="text"
+            id="trip_type"
             name="trip_type"
             value={trip_type}
             onChange={handleChange}
@@ -149,15 +174,17 @@ function PostCreateForm() {
       </Form.Row>
 
       <Form.Group>
-        <Form.Label>Content</Form.Label>
+        <Form.Label htmlFor="content">Content</Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
+          id="content"
           name="content"
           value={content}
           onChange={handleChange}
         />
       </Form.Group>
+      {/* Displaying any about errors */}
       {errors?.content?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
@@ -217,6 +244,7 @@ function PostCreateForm() {
                 ref={imageInput}
               />
             </Form.Group>
+            {/* Displaying any errors with the image */}
             {errors?.image?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
